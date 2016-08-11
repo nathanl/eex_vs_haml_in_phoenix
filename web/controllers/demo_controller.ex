d0035d1ec6479a441b67aafe0ea489e5e1dd2f49
Nathan Long <him@nathanmlong.com>
2016-08-11 16:48:51 -0400

    HAML and EEx templates have identical output

--- web/controllers/demo_controller.ex
+++ web/controllers/demo_controller.ex
@@ -1,11 +1,12 @@
 defmodule HamlComparison.DemoController do
   use HamlComparison.Web, :controller
+  @items [1,2,3,4]
 
   def eex(conn, _params) do
-    render conn, "eex.html", items: [1,2,3]
+    render conn, "eex.html", items: @items
   end
 
   def haml(conn, _params) do
-    render conn, "haml.html", items: [1,2,3]
+    render conn, "haml.html", items: @items
   end
 end
